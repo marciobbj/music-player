5853e6954c24e64e37a404636b41bf10c03bc7f1
Marcio Bernardes <marciobernardes@live.com>
2019-03-26 17:21:10 -0300

    lint no código, adicionada maior interação no frontend

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,53 +1,63 @@
 <template>
-  <div id="app">
-    <h1 class="title">Music Player</h1>
-      <h2 class="subtitle">Make your playlist!!!</h2>
-      <div class="container">
-          <ul style="list-style: none">
-              <Tracks
-                  v-for="track in tracks"
-                  :name="track.name"
-                  :url="track.file" />
-          </ul>
-      </div>
-  </div>
+    <div id="app">
+        <header>
+            <h1>Music Player</h1>
+            <a class="waves-effect waves-light btn-large">Add Song</a>
+        </header>
+        <div class="container">
+            <table class="highlight">
+                <Tracks
+                        v-for="track in tracks"
+                        :name="track.name"
+                        :url="track.file"
+                />
+            </table>
+        </div>
+    </div>
 </template>
 
 <script>
-import Tracks from './components/Tracks.vue'
-import axios from 'axios'
+    import Tracks from './components/Tracks.vue'
+    import axios from 'axios'
 
-export default {
-  name: 'app',
-  components: {
-    Tracks
-  },
-  data: () => {
-    return {
-      tracks: []
+    export default {
+        name: 'app',
+        components: {
+            Tracks,
+        },
+        data: () => {
+            return {
+                tracks: false,
+            }
+        },
+        mounted() {
+            this.getTracks()
+        },
+        methods: {
+            getTracks() {
+                axios.get('/api/v1/tracks').then(response => {
+                    if (response.data.length === 0) {
+                        this.tracks = false
+                        return
+                    }
+                    this.tracks = response.data
+                })
+            },
+        },
     }
-  },
-  mounted(){
-    this.getTracks();
-  },
-  methods: {
-    getTracks () {
-      axios.get('/api/v1/tracks').then(response => {
-        this.tracks = response.data
-      })
-    }
-  },
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin: 0px auto;
-}
+    #app {
+        color: #2c3e50;
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+    }
 
+    table {
+        overflow: auto;
+        max-height: 400px;
+    }
 </style>
